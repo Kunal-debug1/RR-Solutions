13c51ad65a47fdbc74bf26e0504649a98cce9b0a
Kunal-debug1 <Kunalgaikwad.exe@gmail.com>
2026-08-28 22:04:04 +0530

    Configure Next.js static export

---
+++ out/_next/static/chunks/app/blog/page-a2af87ffa56f7ae2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[831],{24:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.bind(n,3384)),Promise.resolve().then(n.bind(n,3404)),Promise.resolve().then(n.bind(n,3873))}},e=>{e.O(0,[481,425,441,255,358],()=>e(e.s=24)),_N_E=e.O()}]);
